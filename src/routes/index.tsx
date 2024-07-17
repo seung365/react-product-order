677e3837a4b7daabff3b66a983af2cf4cfcc73d7
seung365 <bdh3659@naver.com>
2024-07-17 19:33:24 +0900

    feat: product route 추가

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,6 +4,8 @@ import { Layout } from '@/components/features/Layout';
 import { HomePage } from '@/pages/Home';
 import { LoginPage } from '@/pages/Login';
 import { MyAccountPage } from '@/pages/MyAccount';
+import { OrderPage } from '@/pages/Order';
+import { ProductsPage } from '@/pages/Products';
 import { ThemePage } from '@/pages/Theme';
 
 import { PrivateRoute } from './components/PrivateRoute';
@@ -31,6 +33,14 @@ const router = createBrowserRouter([
             element: <MyAccountPage />,
           },
         ],
+      },
+      {
+        path: RouterPath.products,
+        element: <ProductsPage />,
+      },
+      {
+        path: RouterPath.order,
+        element: <OrderPage />,
       },
       {
         path: RouterPath.notFound,
